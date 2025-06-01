7f5886654c00566e507fecfd52d360726a91db32
Yousef Mohsen <ymohsen746@gmail.com>
2025-06-01 21:22:43 +0300

    Editting projects by adding title

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ export default function Sidebar({ onAdd, onClick, projects }) {
           return (
             <li key={index} onClick={() => onClick(index)}>
               <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800 text-stone-400">
-                {project}
+                {project.title}
               </button>
             </li>
           );
